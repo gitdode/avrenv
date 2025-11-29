4a76e49a4259a5dbc592986383b70647bbccfa2b
Torsten Römer <torsten.roemer@luniks.net>
2025-11-29 23:59:07 +0100

    Configure OIDC, implement GET

--- avrenv-web/src/main/java/net/luniks/DataResource.java
+++ avrenv-web/src/main/java/net/luniks/DataResource.java
@@ -1,29 +1,37 @@
 package net.luniks;
 
+import java.util.concurrent.atomic.AtomicReference;
+
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.json.Json;
 import jakarta.json.JsonObject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 @Path("/data")
 public class DataResource {
+    
+    private static final JsonObject EMPTY_JSON = Json.createObjectBuilder().build();
+    
+    private final AtomicReference<JsonObject> current = new AtomicReference<>(EMPTY_JSON);
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonObject get() {
+        return current.get();
     }
 
     @POST
+    @RolesAllowed("baloon")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.TEXT_PLAIN)
-    public Response post(final HttpHeaders headers, final JsonObject json) {
-        System.out.println(headers.getRequestHeader(HttpHeaders.AUTHORIZATION));
+    public Response post(final JsonObject json) {
+        current.set(json);
         System.out.println(json.toString());
 
         return Response.ok().build();
