33b8641163cff0425b07e04442ab76d3e22a1189
Torsten Römer <torsten.roemer@luniks.net>
2025-12-10 22:13:29 +0100

    Script to start Quarkus in dev mode and run client tests

---
+++ avrenv-client/test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+workdir=$(realpath $(dirname "$0"))
+srvdir="$workdir/../avrenv-web"
+command="nohup ./mvnw quarkus:dev"
+url="http://localhost:8080/data"
+
+cd "$srvdir"
+
+nohup $command &
+pid=$!
+
+echo "Waiting for service to be up..."
+
+time=0
+while ([[ $(curl -o /dev/null -s -w "%{http_code}\n" $url) != 200 ]]); do
+    sleep 1;
+    ((time=$time+1))
+    if [[ $time -gt 60 ]]; then
+	 echo "Timeout waiting for service to be up"
+	 kill $pid
+         exit 1
+    fi
+done
+
+cd "$workdir"
+
+make test; test=$?
+
+kill $pid
+
+if [[ $test != 0 ]]; then
+    cat "$srvdir/nohup.out"
+fi
+
+rm "$srvdir/nohup.out"
+
+exit $test
+
